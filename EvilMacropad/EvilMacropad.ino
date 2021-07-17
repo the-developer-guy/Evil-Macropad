1f056ca0e2a344ad69beab6d86f8d2a88d3d6cfd
Durucz <duruczbela@gmail.com>
2021-07-17 21:25:27 +0200

    Basic functionality
    Added basic macropad functionality.
    Added demo functions for simple keypress and combos

--- EvilMacropad/EvilMacropad.ino
+++ EvilMacropad/EvilMacropad.ino
@@ -1,25 +1,102 @@
 #include "DigiKeyboard.h"
 
-#define KEY_DEL 76
+#include "key_definitions.h"
+
+#define PIN_A 5
+#define PIN_B 2
+#define PIN_C 1
+#define PIN_D 0
+
+#define EVIL_A
+#define EVIL_B
+#define EVIL_C
+#define EVIL_D
+
+// chance: 1/x
+#define EVIL_A_CHANCE 1000
+#define EVIL_B_CHANCE 1000
+#define EVIL_C_CHANCE 1000
+#define EVIL_D_CHANCE 1000
+
+
+void keyPressA(void);
+void keyPressB(void);
+void keyPressC(void);
+void keyPressD(void);
+
+unsigned long task;
+bool previousState_a;
+bool previousState_b;
+bool previousState_c;
+bool previousState_d;
+
 // the setup routine runs once when you press reset:
-void setup() {                
-  // initialize the digital pin as an output.
-  pinMode(5, INPUT_PULLUP);
-  pinMode(2, INPUT_PULLUP);
-  pinMode(1, INPUT_PULLUP);
-  pinMode(0, INPUT_PULLUP); 
+void setup() 
+{                
+  // initialize pins (buttons)
+  pinMode(PIN_A, INPUT_PULLUP);
+  pinMode(PIN_B, INPUT_PULLUP);
+  pinMode(PIN_C, INPUT_PULLUP);
+  pinMode(PIN_D, INPUT_PULLUP); 
 
   DigiKeyboard.sendKeyStroke(0);
   
-  // DigiKeyboard.sendKeyStroke(KEY_V , MOD_CONTROL_LEFT); 
-  // DigiKeyboard.sendKeyStroke(KEY_L , MOD_GUI_LEFT);
-  // DigiKeyboard.sendKeyStroke(KEY_DEL , MOD_CONTROL_LEFT | MOD_ALT_LEFT);
+  task = millis() + 1;
+  previousState_a = HIGH;
+  previousState_b = HIGH;
+  previousState_c = HIGH;
+  previousState_d = HIGH;
 }
-// the loop routine runs over and over again forever:
-void loop() {
-  if(digitalRead(5) == LOW) DigiKeyboard.sendKeyStroke(KEY_A);
-  if(digitalRead(2) == LOW) DigiKeyboard.sendKeyStroke(KEY_B);
-  if(digitalRead(1) == LOW) DigiKeyboard.sendKeyStroke(KEY_C);
-  if(digitalRead(0) == LOW) DigiKeyboard.sendKeyStroke(KEY_D);
-  delay(1000);               // wait for a second
+
+void loop() 
+{
+  if(millis() >= task)
+  {
+    task++;
+    bool state_a = digitalRead(PIN_A);
+    bool state_b = digitalRead(PIN_B);
+    bool state_c = digitalRead(PIN_C);
+    bool state_d = digitalRead(PIN_D);
+
+    if(state_a == LOW && previousState_a == HIGH)
+    {
+      keyPressA();
+    }
+    if(state_b == LOW && previousState_b == HIGH)
+    {
+      keyPressB();
+    }
+    if(state_c == LOW && previousState_c == HIGH) 
+    {
+      keyPressC();
+    }
+    if(state_d == LOW && previousState_d == HIGH) 
+    {
+      keyPressD();
+    }
+
+    previousState_a = state_a;
+    previousState_b = state_b;
+    previousState_c = state_c;
+    previousState_d = state_d;
+  }
 }
+
+void keyPressA(void)
+{
+  DigiKeyboard.sendKeyStroke(KEY_A);
+}
+
+void keyPressB(void)
+{
+  DigiKeyboard.sendKeyStroke(KEY_V , MOD_CONTROL_LEFT);
+}
+
+void keyPressC(void)
+{
+  DigiKeyboard.sendKeyStroke(KEY_L , MOD_GUI_LEFT);
+}
+
+void keyPressD(void)
+{
+  DigiKeyboard.sendKeyStroke(KEY_DEL , MOD_CONTROL_LEFT | MOD_ALT_LEFT);
---
+++ EvilMacropad/key_definitions.h
@@ -0,0 +1,1 @@
+#define KEY_DEL 76
